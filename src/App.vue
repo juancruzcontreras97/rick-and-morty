4d654186a893834e35e62e4c4661b64d04296d76
juan.contreras <juan.contreras>
2023-03-17 15:45:54 -0300

    set & get store y valores de variables inicial

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<h1>Rick & Morty</h1>
+	<ListCharacters></ListCharacters>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import ListCharacters from "./components/ListCharacters";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: "App",
+		components: { ListCharacters },
+	};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
---
+++ src/components/ListCharacters.vue
@@ -0,0 +1,21 @@
+<template>
+	<section>
+		<div class="characters">
+			<div class="characters_item" v-for="character in characters" :key="character.id">
+				{{ character.name }}
+			</div>
+		</div>
+	</section>
+</template>
+<script setup>
+	import { onMounted, computed } from "vue";
+	import { useStore } from "vuex";
+	const store = useStore();
+	const characters = computed(() => {
+		return store.state.charactersFilter;
+	});
+	onMounted(() => {
+		store.dispatch("getCharacters");
+	});
+</script>
+<style lang=""></style>
